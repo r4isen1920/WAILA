50093341b57ca7ff00af7f475eb910968142fb80
r4isen1920 <me@r4isen.net>
2025-10-21 22:51:15 +0800

    Scripts: remove TS compiled emit

--- packs/data/gametests/scripts/main.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("./String");
-require("./waila/Settings");
-require("./waila/Waila");
-const stylish_1 = require("@bedrock-oss/stylish");
-(0, stylish_1.init)();
--- packs/data/gametests/scripts/types/LookAtObjectTypeEnum.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.LookAtObjectTypeEnum = void 0;
-var LookAtObjectTypeEnum;
-(function (LookAtObjectTypeEnum) {
-    LookAtObjectTypeEnum["ENTITY"] = "entity";
-    LookAtObjectTypeEnum["TILE"] = "tile";
-})(LookAtObjectTypeEnum || (exports.LookAtObjectTypeEnum = LookAtObjectTypeEnum = {}));
